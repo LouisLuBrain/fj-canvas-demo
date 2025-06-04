8020ab6a843ac2702af028cf2502609c5129596b
yizhou <1394853556@qq.com>
2025-06-04 17:05:35 +0800

    chore: support load svg file

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,11 @@
 /// <reference types="vite/client" />
+
+declare module '*.svg' {
+    const content: string;
+    export default content;
+}
+
+declare module '*.module.css' {
+    const classes: { [key: string]: string };
+    export default classes;
+}
