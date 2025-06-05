73f68693ce3db5e4bce8e87a2763fe7e3b2bb419
yizhou <1394853556@qq.com>
2025-06-05 16:12:13 +0800

    fix: fix scale size calc

--- src/components/canvas/FJCanvasUtils.ts
+++ src/components/canvas/FJCanvasUtils.ts
@@ -271,8 +271,8 @@ export class FJCanvasUtils {
         offscreen.height = image.naturalHeight * dpr;
 
         // 计算缩放比例（基于显示尺寸）
-        const scaleX = this.canvas.width / (image.naturalWidth * dpr - CANVAS_FIT_PADDING_X * dpr);
-        const scaleY = this.canvas.height / (image.naturalHeight * dpr - CANVAS_FIT_PADDING_Y * dpr);
+        const scaleX = this.canvas.width / (image.naturalWidth * dpr - CANVAS_FIT_PADDING_X * 2 * dpr);
+        const scaleY = this.canvas.height / (image.naturalHeight * dpr - CANVAS_FIT_PADDING_Y * 2 * dpr);
         const scaleToFit = Math.min(scaleX, scaleY);
         this._scaleToFit = scaleToFit;
 
