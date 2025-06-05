dedc032f23931f52ba7c3e6cb77175de3d00b512
yizhou <1394853556@qq.com>
2025-06-05 14:00:58 +0800

    feat: add history image list

--- src/components/imagesHistoryList/FJImageHistoryList.tsx
+++ src/components/imagesHistoryList/FJImageHistoryList.tsx
@@ -1,3 +1,33 @@
+import styles from './imageHistoryList.module.css';
+
+import image1 from '../../assets/alexander-mass-ohVkrmTwirg-unsplash.jpg';
+import image2 from '../../assets/andrey-k-FUKidTAPUdk-unsplash.jpg';
+import image3 from '../../assets/luca-ercolani-pPLKOwxVmfE-unsplash.jpg';
+import image4 from '../../assets/anna-spoljar-dfaCZjYYNTE-unsplash.jpg';
+import image5 from '../../assets/daniel-akselrod-GRgfv49Nf-M-unsplash.jpg';
+
+const IMAGE_HISTORY_LIST = [
+    { imageURL: image1, id: crypto.randomUUID() },
+    { imageURL: image2, id: crypto.randomUUID() },
+    { imageURL: image3, id: crypto.randomUUID() },
+    { imageURL: image4, id: crypto.randomUUID() },
+    { imageURL: image5, id: crypto.randomUUID() },
+];
+
 export default function FJImageHistoryList() {
-    return <div>FJImageHistoryList</div>;
+    return (
+        <div className={styles['images-history-list-container']}>
+            <div className={styles['images-history-list']}>
+                {IMAGE_HISTORY_LIST.map(item => (
+                    <div className={styles['images-history-list-item']} key={item.id}>
+                        <img
+                            className={styles['images-history-list-item-image']}
+                            src={item.imageURL}
+                            alt='history image'
+                        />
+                    </div>
+                ))}
+            </div>
+        </div>
+    );
 }
