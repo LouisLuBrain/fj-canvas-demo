152c9f881553059bf87082d07d5ae6381ac7c15a
yizhou <1394853556@qq.com>
2025-06-05 10:17:07 +0800

    feat: add remove object button and update styles

--- src/components/imageUploader/FJImageUploader.tsx
+++ src/components/imageUploader/FJImageUploader.tsx
@@ -1,19 +1,32 @@
-import { useCallback, useState } from 'react';
+import { useMemo, useState } from 'react';
 import styles from './imageUploader.module.css';
+import fjUploadIcon from '../../assets/fj-upload-icon.svg';
 import { IconUpload } from '@tabler/icons-react';
+import commonStyles from '../../common.module.css';
+
+const ACCEPT_IMAGE_FORMAT = [
+    'image/png',
+    'image/jpeg',
+    'image/jpg',
+    'image/webp',
+    'image/jfif',
+    'image/tiff',
+    'image/bmp',
+];
 
 interface FJImageUploaderProps {
     onImageUpload?: (image: HTMLImageElement) => void;
 }
 
 export default function FJImageUploader(props: FJImageUploaderProps) {
-    // const [isLoading, setIsLoading] = useState(false);
+    const [isDragging, setIsDragging] = useState(false);
     const [image, setImage] = useState<HTMLImageElement | null>(null);
 
-    const handleUploadImage = useCallback((e: React.ChangeEvent<HTMLInputElement>) => {
-        const file = e.target.files?.[0];
-        if (!file) return;
+    const isImageUploaded = useMemo(() => {
+        return image !== null && image.src !== '';
+    }, [image]);
 
+    const handleUploadImage = (file: File) => {
         const url = URL.createObjectURL(file);
         const img = new Image();
         img.src = url;
@@ -21,19 +34,84 @@ export default function FJImageUploader(props: FJImageUploaderProps) {
             props.onImageUpload?.(img);
             setImage(img);
         };
-        img.onerror = e => {
-            console.error(e);
-        };
-    }, []);
-    return image ? (
-        <div></div>
-    ) : (
-        <label className={styles['image-uploader']} title='Upload image'>
-            <div className={styles['image-uploader-icon']}>
-                <IconUpload size={20} />
-            </div>
-            upload image
-            <input aria-label='Upload image' type='file' accept='image/*' onChange={handleUploadImage} />
+    };
+
+    const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        const file = event.target.files?.[0];
+        if (file) {
+            handleUploadImage(file);
+        }
+    };
+
+    const handleDragEnter = (e: React.DragEvent) => {
+        e.preventDefault();
+        e.stopPropagation();
+        setIsDragging(true);
+    };
+
+    const handleDragLeave = (e: React.DragEvent) => {
+        e.preventDefault();
+        e.stopPropagation();
+        setIsDragging(false);
+    };
+
+    const handleDragOver = (e: React.DragEvent) => {
+        e.preventDefault();
+        e.stopPropagation();
+        setIsDragging(true);
+    };
+
+    const handleDrop = (e: React.DragEvent) => {
+        e.preventDefault();
+        e.stopPropagation();
+        setIsDragging(false);
+
+        const files = Array.from(e.dataTransfer.files);
+        files.map(file => {
+            if (ACCEPT_IMAGE_FORMAT.includes(file.type)) {
+                handleUploadImage(file);
+            } else {
+                console.error('Unsupported format. Please upload images in jpg, png, etc.');
+            }
+        });
+    };
+
+    return (
+        <label
+            className={`${styles['image-uploader']} ${isDragging ? styles['dragging'] : ''}`}
+            onDragEnter={handleDragEnter}
+            onDragLeave={handleDragLeave}
+            onDragOver={handleDragOver}
+            onDrop={handleDrop}
+            data-is-uploaded={isImageUploaded}
+        >
+            {isImageUploaded ? (
+                <>
+                    <div className={styles['image-uploader-uploaded-icon']}>
+                        <IconUpload size={18} />
+                    </div>
+                    <span className={styles['image-uploader-uploaded-text']}>Upload Up to 10 Images</span>
+                </>
+            ) : (
+                <>
+                    <div className={styles['image-uploader-icon']}>
+                        <img src={fjUploadIcon} alt='upload' />
+                    </div>
+                    <span className={styles['image-uploader-text']}>
+                        {isImageUploaded ? 'Upload Up to 10 Images' : 'Click or drag to upload'}
+                    </span>
+                    <div className={`${commonStyles['fj-main-btn']} ${styles['image-uploader-btn']}`}>Upload Image</div>
+                </>
+            )}
+
+            <input
+                type='file'
+                accept={ACCEPT_IMAGE_FORMAT.join(',')}
+                onChange={handleFileChange}
+                style={{ display: 'none' }}
+                multiple
+                max={10}
+            />
         </label>
     );
 }
--- src/containers/FJAIObjectRemove.container.tsx
+++ src/containers/FJAIObjectRemove.container.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useMemo, useState } from 'react';
 import FJImageHistoryList from '../components/imagesHistoryList/FJImageHistoryList';
 import FJHeader from '../components/header/FJHeader';
 import styles from './container.module.css';
@@ -6,16 +6,42 @@ import FJCanvasTool from '../components/canvas/FJCanvasTool';
 import FJImageUploader from '../components/imageUploader/FJImageUploader';
 import FJCanvasDesk from '../components/canvas/FJCanvasDesk';
 
+import FJCoinIcon from '../assets/fj-coin.png';
+import commonStyles from '../common.module.css';
+
 export default function FJAIObjectRemoveContainer() {
     const [tab, setTab] = useState<'canvas' | 'history'>('canvas');
+    const [image, setImage] = useState<HTMLImageElement | null>(null);
+
+    const isImageUploaded = useMemo(() => {
+        return image !== null && image.src !== '';
+    }, [image]);
+
+    const handleRemoveObject = () => {
+        // TODO: call remove object api here
+    };
 
     return (
         <div className={styles.container}>
             <FJHeader />
             <div className={styles.content}>
                 <div className={styles['left-content']}>
-                    <FJImageUploader />
-                    <FJCanvasTool />
+                    <FJImageUploader onImageUpload={setImage} />
+                    {isImageUploaded && <FJCanvasTool />}
+                    {isImageUploaded && (
+                        <button
+                            type='button'
+                            aria-label='remove object'
+                            onClick={handleRemoveObject}
+                            className={`${commonStyles['fj-main-btn']} ${styles['remove-object-btn']}`}
+                        >
+                            Remove
+                            <span>
+                                <img src={FJCoinIcon} width={16} height={16} alt='cost coin' />
+                                -3
+                            </span>
+                        </button>
+                    )}
                 </div>
                 <div className={styles['right-content']}>
                     <div>
