ec65ea4012610c0ba6606d0162f5017a5568bfd3
yizhou <1394853556@qq.com>
2025-06-10 18:21:54 +0800

    fix: try fancy tab buttons

--- src/components/tab/FJTab.tsx
+++ src/components/tab/FJTab.tsx
@@ -1,3 +1,4 @@
+import { useLayoutEffect, useState } from 'react';
 import styles from './tab.module.css';
 
 export interface Tab {
@@ -13,8 +14,27 @@ interface FJTabProps {
 }
 
 export default function FJTab({ tabs, currentTab, onTabChange }: FJTabProps) {
+    const [activeTabWidth, setActiveTabWidth] = useState(0);
+    const [activeTabOffsetLeft, setActiveTabOffsetLeft] = useState(0);
+
+    useLayoutEffect(() => {
+        console.log('LOG ===> currentTab: ', currentTab);
+        const btn = document.querySelector(`.${styles['tab-button']}[data-active='true']`);
+        console.log('LOG ===> btn: ', btn);
+        setActiveTabWidth(btn?.clientWidth || 0);
+        setActiveTabOffsetLeft(btn?.scrollLeft || 0);
+    }, [currentTab]);
+
     return (
-        <div className={styles['tab-container']}>
+        <div
+            className={styles['tab-container']}
+            style={
+                {
+                    '--active-tab-width': activeTabWidth + 'px',
+                    '--active-tab-offset-left': activeTabOffsetLeft + 'px',
+                } as React.CSSProperties
+            }
+        >
             {tabs.map(tab => (
                 <button
                     data-active={tab.value === currentTab?.value}
